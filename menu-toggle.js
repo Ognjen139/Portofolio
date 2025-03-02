24f6a44815d85d37ac2990a98999fa38c02c993b
Ognjen139 <mrinessential@hotmail.com>
2025-03-02 19:58:47 +0100

    menu-toggle.js

--- menu-toggle.js
+++ menu-toggle.js
@@ -1,6 +1,8 @@
-const menuButton = document.querySelector('.menu-button'); // Dodaj dugme za otvaranje menija
-const mobileMenu = document.querySelector('.mobile-menu');
+// Pronadji dugme za otvaranje menija (hamburger) i meni
+const menuButton = document.querySelector('.hamburger');
+const mobileMenu = document.getElementById('mobileMenu'); // Meni ima ID 'mobileMenu'
 
+// Dodaj event listener za klik na hamburger
 menuButton.addEventListener('click', () => {
-  mobileMenu.classList.toggle('active');
+  mobileMenu.classList.toggle('active'); // Dodaj ili ukloni klasu 'active' za prikazivanje menija
 });
